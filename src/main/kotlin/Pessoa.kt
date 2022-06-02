0c9bd1e8a0235e9428b21b75ce20c78df9c5d313
Renato Botini <botinirenato@gmail.com>
2022-06-02 19:39:20 -0300

    Criado a Primeira Classe Pessoa

---
+++ src/main/kotlin/Pessoa.kt
@@ -0,0 +1,13 @@
+class Pessoa {
+
+    var nome : String = "Renato"
+    var idade: Int = 38
+}
+
+fun main() {
+
+    var pessoa = Pessoa()
+
+    println("Seu nome é ${pessoa.nome} sua idade: ${pessoa.idade}")
+
+}
