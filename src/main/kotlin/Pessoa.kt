6d5763522f271b79bceda4d858b83510e29b6ef8
Renato Botini <botinirenato@gmail.com>
2022-06-02 20:01:11 -0300

    Alterado o modificado de acesso do atributo cpf para privado

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,13 +1,17 @@
 class Pessoa {
 
     var nome : String = "Renato"
+
     var idade: Int = 38
+
+    var cpf:String = "35583337800"
+    private  set
 }
 
 fun main() {
 
     var pessoa = Pessoa()
-
     println("Seu nome é ${pessoa.nome} sua idade: ${pessoa.idade}")
+    println(pessoa.cpf)
 
 }
